f67bcfa796d464154fd713f37c2b2bbc80863df2
gumi99 <yewongumi@gmail.com>
2024-05-28 17:53:30 +0900

    제출

--- js/common.js
+++ js/common.js
@@ -7,8 +7,14 @@ $(document).ready(function(){//시작
     $(".planet3 .planet3-img img:first-child").hide();
   });
 
-  $(".planet3 .planet3-img img:last-child").click(function(){
+  $(".planet3 .planet3-img img:first-child").click(function(){
+    $(".planet3 .planet3-img img:last-child").fadeIn();
     $(this).hide();
+    $(".planet3 .planet3-click a").hide();
+  });
+
+  $(".planet3 .planet3-img img:last-child").click(function(){
+    $(this).fadeOut();
     $(".planet3 .planet3-img img:first-child").fadeIn();
     $(".planet3 .planet3-click a").fadeIn();
   });
